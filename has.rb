37436cc9a19d323ddea869e9d20c6ee214177fe1
Kunal Dabir <kunal.dabir@gmail.com>
2023-07-28 21:08:17 +0530

    update version

--- has.rb
+++ has.rb
@@ -1,8 +1,8 @@
 class Has < Formula
   desc "checks presence of various command line tools and their versions on the path"
   homepage "https://github.com/kdabir/has"
-  url "https://github.com/kdabir/has/archive/v1.4.0.tar.gz"
-  sha256 "0e73552dbf59e3da0d9254da87c94599595c9ea07c69a62a4853e69bbf3f0d7d"
+  url "https://github.com/kdabir/has/archive/v1.5.0.tar.gz"
+  sha256 "da39a3ee5e6b4b0d3255bfef95601890afd80709"
 
   def install
     bin.install "has"
@@ -10,6 +10,6 @@ class Has < Formula
 
   test do
      system bin/"has", "has"
-     # assert_equal %x('#{bin}/has'), "✔ has 1.4.0"
+     #assert_equal %x('#{bin}/has'), "✔ has 1.5.0"
   end
 end
