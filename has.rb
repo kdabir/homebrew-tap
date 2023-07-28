7f029aea097d858fd271a9355958be08e8d73dde
Kunal Dabir <kunal.dabir@gmail.com>
2023-07-28 21:16:06 +0530

    Update has.rb

--- has.rb
+++ has.rb
@@ -2,7 +2,7 @@ class Has < Formula
   desc "checks presence of various command line tools and their versions on the path"
   homepage "https://github.com/kdabir/has"
   url "https://github.com/kdabir/has/archive/v1.5.0.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "d45be15f234556cdbaffa46edae417b214858a4bd427a44a2a94aaa893da7d99"
 
   def install
     bin.install "has"
