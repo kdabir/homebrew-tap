339f38e2f8ebe4cec8dcf68b2a32cf2dda5d8f50
Kunal Dabir <kunal.dabir@gmail.com>
2025-07-29 05:06:15 -0400

    script to updated formula

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+REPO="kdabir/has"
+FORMULA_PATH="has.rb"
+
+# Get latest tag from GitHub API
+LATEST_TAG=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | jq -r .tag_name)
+
+# Download tarball and get sha256
+TARBALL_URL="https://github.com/$REPO/archive/$LATEST_TAG.tar.gz"
+SHA256=$(curl -L "$TARBALL_URL" | shasum -a 256 | awk '{print $1}')
+
+# Update formula file
+sed -i "" "s|url \".*\"|url \"$TARBALL_URL\"|" "$FORMULA_PATH"
+sed -i "" "s|sha256 \".*\"|sha256 \"$SHA256\"|" "$FORMULA_PATH"
