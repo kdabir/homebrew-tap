1e82cb448fd150e5fbb67cfb631efae95e69c060
Kunal Dabir <kunal@appcd.com>
2025-07-29 14:47:50 +0530

    make sed work across mac and linux

--- update.sh
+++ update.sh
@@ -9,6 +9,14 @@ LATEST_TAG=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | jq -
 TARBALL_URL="https://github.com/$REPO/archive/$LATEST_TAG.tar.gz"
 SHA256=$(curl -L "$TARBALL_URL" | shasum -a 256 | awk '{print $1}')
 
+
+# Detect OS and set sed options
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SED_INPLACE=(-i "")
+else
+  SED_INPLACE=(-i)
+fi
+
 # Update formula file
-sed -i "" "s|url \".*\"|url \"$TARBALL_URL\"|" "$FORMULA_PATH"
-sed -i "" "s|sha256 \".*\"|sha256 \"$SHA256\"|" "$FORMULA_PATH"
+sed "${SED_INPLACE[@]}" "s|url \".*\"|url \"$TARBALL_URL\"|" "$FORMULA_PATH"
+sed "${SED_INPLACE[@]}" "s|sha256 \".*\"|sha256 \"$SHA256\"|" "$FORMULA_PATH"
